ea5bef69bcba602ed890052d3c43999afce879da
Sao Kanneh <s3788996@student.rmit.edu.au>
2021-05-16 17:04:44 +1000

    overloaded input & output operators in the Tile class

--- Tile.cpp
+++ Tile.cpp
@@ -3,6 +3,11 @@
 
 // Empty... for now?
 
+Tile::Tile(Colour colour, Shape shape) {
+    this->colour = colour;
+    this->shape = shape;
+}
+
 Tile::Tile() {
     // do nothing
 }
@@ -15,3 +20,19 @@ Tile::Tile(Tile& other) {
 Shape Tile::getShape() {
     return this->shape;
 }
+
+ostream& operator<<(ostream& os, const Tile& t) {
+    os << t.colour << t.shape;
+}
+
+ofstream& operator<<(ofstream& of, const Tile&t) {
+    of << t.colour << t.shape;
+}
+
+ifstream& operator>>(ifstream& in, Tile& t) {
+    char c;
+    char s;
+    in >> t.colour;
+    in >> t.shape;
+}
+
