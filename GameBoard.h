c0bf6d598d8a2725a808c7eebf6b80b331d2f96d
Muhammed Khodr <s3779580@student.rmit.edu.au>
2021-05-16 18:50:25 +1000

    Game Board Print and vector impementation

---
+++ GameBoard.h
@@ -0,0 +1,23 @@
+#ifndef ASSIGN2_GameBoard_H
+#define ASSIGN2_GameBoard_H
+
+#include <iostream>
+#include "LinkedList.h"
+#include <vector>
+
+#define MAXIMUM_BOARD_SIZE      26
+class GameBoard{
+    
+    public:
+    GameBoard();
+    // ~GameBoard();
+    // void TileInsert(char position[2], Tile tile );
+    friend std::ostream& operator << (std::ostream& os, const GameBoard g);
+    private:
+    std::vector<Tile*> gameRow;
+    std::vector<std::vector<Tile*>> gameBoard;
+    
+
+};
+
+#endif // ASSIGN2_GameBoard_H;
