5f6943c549695d63eef11f297f33dd487eb91334
Patrick <s3508768@student.rmit.edu.au>
2021-05-16 18:29:17 +1000

    Menu Feature

---
+++ Menu.h
@@ -0,0 +1,30 @@
+#ifndef ASSIGN2_MENU_H
+#define ASSIGN2_MENU_H
+#include <iostream>
+#include "Menu.h"
+#include "LinkedList.h"
+#include "Node.h"
+#include <regex>
+using std::string;
+class Menu {
+
+public:
+Menu();
+void runGame();
+void startNewGame();
+void loadGame();
+
+void displayMenu();
+void loadCredits();
+
+bool checkValidName(std::string player1);
+std::string inputPlayerName(string player);
+
+
+
+
+
+
+
+};
+#endif //ASSIGN_MENU_H
